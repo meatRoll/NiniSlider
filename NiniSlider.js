2f39828e165c6071f78eebbcba500069d8c12ad2
Meatroll <chenshiman@foxmail.com>
2017-12-15 17:56:28 +0800

    nice

--- NiniSlider.js
+++ NiniSlider.js
@@ -30,7 +30,7 @@
     // 默认参数和工厂函数
     var version = "1.0.0"
     , document = window.document
-    , slider = {}
+    , slider
     , NiniSlider = function(options) {
         var i = 0
         , optName
@@ -59,6 +59,8 @@
         , id
         , data
         , styleTag
+        , sliderWidth
+        , slidersContainer
         , picHtml = ""
         , dotHtml = ""
         , insertHtml = ""
@@ -69,6 +71,8 @@
         id = opts.id;
         data = opts.data;
         el = slider.el = document.getElementById(id);
+        sliderWidth = slider.width = NiniSlider.css(el, "width");
+        slider.height = NiniSlider.css(el, "height");
         // 添加dom结构
         NiniSlider.each(data, function(index, value) {
             picHtml +=  "<li><img src=\"" + (value.src?value.src:"") + "\" alt=\"\"></li>";
@@ -78,10 +82,18 @@
                    + "<div class=\"pagination\" id=\"" + id + "Pagination\"><ul class=\"pagination-container clearfix\">" + dotHtml + "</ul></div>";
         el.innerHTML = insertHtml;
         // 调整样式
-        styleText = "#" + id + " nbsp;div,#" + id + " ul,#" + id + " li,#" + id + " img{box-sizing:border-box;padding: 0;margin: 0;}#" + id + "{overflow:hidden;position:relative;}#" + id + " .clearfix{zoom:1;}#" + id + " .clearfix::after{content:\"\";display:table;clear:both;}#" + id + " .sliders-container{height:100%;list-style:none;}#test .sliders-container>li{height:100%;float:left;}#" + id + " .pagination{width:100%;position:absolute;left:0;bottom:5%;}#" + id + " .pagination-container{margin:0 auto;}#" + id + " .dot{width:20px;height:20px;float:left;border-radius:50%;background-color:red;cursor:pointer;}";
+        slidersContainer = document.getElementById(id + "SlidersContainer");
+        NiniSlider.css(slidersContainer, { width: sliderWidth.replace("px", "") * data.length + "px"});
+        styleText = "#" + id + " div,#" + id + " ul,#" + id + " li,#" + id + " img{box-sizing:border-box;padding: 0;margin: 0;}#" + id + " ul{margin:0;padding:0;}#" + id + "{overflow:hidden;position:relative;}#" + id + " .clearfix{zoom:1;}#" + id + " .clearfix::after{content:\"\";display:table;clear:both;}#" + id + " .sliders-container{height:100%;}#" + id + " ul>li{height:100%;float:left;list-style:none;}#" + id + " .pagination-container>li+li{margin-left:10px}#" + id + " .pagination{width:100%;position:absolute;left:0;bottom:5%;}#" + id + " .pagination-container{margin:0 auto;}#" + id + " .dot{width:20px;height:20px;float:left;border-radius:50%;background-color:red;cursor:pointer;}";
         styleTag = document.createElement("style");
-        styleTag.innerHTML = styleText;
-        
+        if ('styleSheet' in styleTag) {
+            styleTag.setAttribute('type', 'text/css');
+            styleTag.styleSheet.cssText = styleText;
+        } else {
+            styleTag.innerHTML = styleText;
+        }
+        document.getElementsByTagName("head")[0].appendChild(styleTag);
+
     } 
 
     // 扩展函数(只提供浅拷贝)
@@ -107,6 +119,7 @@
 
     // 增加方法
     NiniSlider.extend(NiniSlider, {
+        // 遍历
         each: function(target, callback) {
             if(!(target instanceof Array)){
                 throw new Error("传入目标类型错误，不能为" + target);
@@ -115,6 +128,25 @@
             , length = target.length;
             for(; i < length; i++){
                 callback(i, target[i]);
+            }
+        },
+        // 获取样式与赋予样式
+        css: function(target, obj) {
+            if(typeof obj === "string") {
+                var style = window.getComputedStyle && window.getComputedStyle(target) || target.currentStyle;
+                return style[obj];
+            }
+            for(var style in obj) {
+                target.style[style] = obj[style];
+            }
+        },
+        //获取文本与赋予文本
+        text: function(target, text) {
+            if(arguments.length === 1) {
+                return target.textContent? target.textContent: target.innerText;
+            }
+            if(arguments.length > 1) {
+                target.textContent? target.textContent = text: target.innerText = text;
             }
         }
     });
